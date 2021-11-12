b92d34b3b20184b768caa0d69c13bca7336c663b
Orazio <orazioventrella@hotmail.it>
2021-11-12 15:41:37 +0100

    v-for

---
+++ js/main.js
@@ -0,0 +1,38 @@
+var elencoTask = new Vue ({
+
+    el = "#myApp",
+
+    data : {
+
+        tasks: [
+
+            {
+                text: "cucinare",
+                done: false
+            },
+
+            {
+                text: "Fare la spesa",
+                done: true
+            },
+
+            {
+                text: "Fare il bucato",
+                done: false
+            },
+
+            {
+                text: "Studiare",
+                done: false
+            },
+
+            {
+                text: "uscire il cane",
+                done: true
+            }
+
+        ]
+    }
+    
+}
+);
